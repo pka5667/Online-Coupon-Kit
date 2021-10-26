bcca215d9fa4597ad347ef5a33f00d2e9509b0f0
Pulkit Kumar Agarwal <63954083+pka5667@users.noreply.github.com>
2021-10-26 11:53:07 +0530

    REST Api caches added

--- api/views.py
+++ api/views.py
@@ -1,10 +1,21 @@
+from django.utils.decorators import method_decorator
 from core.models import Udemy_Course
 from .serializers import UdemyCourseSerializer
 from rest_framework import viewsets
-
+from rest_framework.views import Response
+from django.views.decorators.cache import cache_page
 
 
 # VIEW FUNCTION
-class UdemyCourseReadOnlyViewSet(viewsets.ModelViewSet):
-    queryset = Udemy_Course.objects.all().order_by('-last_updated')[:10]
-    serializer_class = UdemyCourseSerializer
+class UdemyCourseReadOnlyViewSet(viewsets.ViewSet):
+    @method_decorator(cache_page(60*60*24))
+    def list(self, request):
+        courses = Udemy_Course.objects.all().order_by('-last_updated')[:20]
+        serializer = UdemyCourseSerializer(courses, many=True)
+        return Response(serializer.data)
+
+    @method_decorator(cache_page(60*60*24))
+    def retrieve(self, request, pk):
+        courses = Udemy_Course.objects.filter(model_title__icontains=pk).order_by('-last_updated')[:10]
+        serializer = UdemyCourseSerializer(courses, many=True)
+        return Response(serializer.data)
