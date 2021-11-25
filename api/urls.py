18e12d95ce2cfd05e2c2ca0f24d65cafbfc50922
Pulkit Kumar Agarwal <63954083+pka5667@users.noreply.github.com>
2021-11-25 09:48:12 +0530

    Sitemap Cache added

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('udmey_course', views.UdemyCourseReadOnlyViewSet, basename="udmey_course_api")
+router.register('udemy_course', views.UdemyCourseReadOnlyViewSet, basename="udemy_course_api")
 
 
 urlpatterns = [
--- core/sitemaps.py
+++ core/sitemaps.py
@@ -1,8 +1,11 @@
 from django.contrib.sitemaps import Sitemap
 from .models import Udemy_Course
+from django.core.cache import cache
 
 
 class UdemyCourseSitemap(Sitemap):
     def items(self):
-        return Udemy_Course.objects.all().order_by('-last_updated')
+        if not cache.get("sitemap_udemy_courses"):
+            cache.set("sitemap_udemy_courses", Udemy_Course.objects.all().order_by('-last_updated'), timeout=500000)
+        return cache.get("sitemap_udemy_courses")
 
